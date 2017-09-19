2fa35d7b16d6f462603ef978fee29fede8553143
Ricardo Maximino <ricardomaximino@hotmail.com>
2017-09-19 15:31:01 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { AsideComponent } from './aside/aside.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BodyComponent,
+    AsideComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
